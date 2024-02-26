02644bff98594257a2a20a47ad81228534200b15
kwinkich <vankord21@gmail.com>
2024-02-26 21:01:11 +0200

    added LastUpdate Section and Card, styles any components

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,24 @@
 import { BlogCard } from '../components/BlogCard/BlogCard';
 import { Header } from '../components/Header/Header';
+import { LastUpdates } from '../components/LastUpdates/LastUpdates';
 
 export default function MainPage() {
 	return (
-		<main className='container mx-auto max-w-[60%]'>
+		<main className='container mx-auto max-w-[65%]'>
 			<Header />
-			<div className=''>
-				<h1 className='text-4xl text-white font-bold mb-10'>Articles</h1>
-				<div className='max-w-max grid lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-x-16 gap-y-16'>
-					<BlogCard />
-					<BlogCard />
-					<BlogCard />
-					<BlogCard />
-					<BlogCard />
-					<BlogCard />
+			<div className='flex justify-between'>
+				<div>
+					<h1 className='text-4xl text-white font-bold mb-10'>Articles</h1>
+					<div className='max-w-max grid lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-x-16 gap-y-16'>
+						<BlogCard />
+						<BlogCard />
+						<BlogCard />
+						<BlogCard />
+						<BlogCard />
+						<BlogCard />
+					</div>
 				</div>
+				<LastUpdates />
 			</div>
 		</main>
 	);
