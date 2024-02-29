f73d04943c528181229ebf879951612c7431d202
kwinkich <vankord21@gmail.com>
2024-02-29 19:46:36 +0200

    added edit solution for post and news

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Button } from '../components/Button/Button';
 import { Header } from '../components/Header/Header';
+import { Input } from '../components/Input/Input';
+import { Label } from '../components/Label/Label';
 import { useNews } from '../contexts/NewsContext';
+
 export default function NewsPage() {
+	const [isEdit, setIsEdit] = useState<boolean>(false);
+	const [newsName, setNewsName] = useState<string>('');
+	const [newsDescription, setNewsDescription] = useState<string>('');
 	const { id } = useParams();
-	const { newss } = useNews();
+	const { newss, editNews } = useNews();
 
 	const news = id
 		? newss.find((news) => news.id === parseInt(id, 10))
 		: undefined;
+
+	function handleEditNews() {
+		const editedNews = {
+			name: newsName,
+			description: newsDescription,
+			id: news?.id || 0,
+		};
+		editNews(editedNews);
+		setIsEdit(false);
+	}
 
 	return (
 		<section className='container mx-auto max-w-[65%]'>
@@ -17,9 +35,32 @@ export default function NewsPage() {
 					<p className='text-lg text-gray-200 mb-5'> back</p>
 				</Link>
 				<h1 className='text-4xl text-white font-bold mb-10'>{news?.name}</h1>
-				<div>
-					<p className='text-xl text-white'>{news?.description}</p>
-				</div>
+				{!isEdit ? (
+					<>
+						<div>
+							<p className='text-xl text-white mb-5'>{news?.description}</p>
+						</div>
+						<Button click={() => setIsEdit(true)}>Edit</Button>
+					</>
+				) : (
+					<div className='flex flex-col gap-y-4 max-w-[40%]'>
+						<div className='flex items-center gap-x-3 justify-between '>
+							<Label labelContent='News name' />
+							<Input
+								placeholder='News Name'
+								onChange={(e) => setNewsName(e.target.value)}
+							/>
+						</div>
+						<div className='flex items-center gap-x-3 justify-between'>
+							<Label labelContent='News Content' />
+							<Input
+								placeholder='News Content'
+								onChange={(e) => setNewsDescription(e.target.value)}
+							/>
+						</div>
+						<Button click={handleEditNews}>Ok</Button>
+					</div>
+				)}
 			</div>
 		</section>
 	);
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Button } from '../components/Button/Button';
 import { Header } from '../components/Header/Header';
+import { Input } from '../components/Input/Input';
+import { Label } from '../components/Label/Label';
 import { Tag } from '../components/Tag/Tag';
 import { usePost } from '../contexts/PostContext';
 
 export default function PostPage() {
+	const [postName, setPostName] = useState<string>('');
+	const [postDescription, setPostDescription] = useState<string>('');
+	const [postTags, setPostTags] = useState<string[]>([]);
+	const [isEdit, setIsEdit] = useState<boolean>(false);
 	const { id } = useParams();
-	const { posts } = usePost();
+	const { posts, editPost } = usePost();
 
 	const post = id
 		? posts.find((post) => post.id === parseInt(id, 10))
 		: undefined;
+
+	const handleEditPost = () => {
+		const editedPost = {
+			name: postName,
+			description: postDescription,
+			id: post?.id || 0,
+			tags: postTags,
+		};
+		editPost(editedPost);
+		setIsEdit(false);
+	};
 
 	return (
 		<section className='container mx-auto max-w-[65%]'>
@@ -19,18 +38,48 @@ export default function PostPage() {
 					<p className='text-lg text-gray-200 mb-5'> back</p>
 				</Link>
 				<h1 className='text-4xl text-white font-bold mb-10'>{post?.name}</h1>
-				<div>
-					<p className='text-xl text-white mb-10'>{post?.description}</p>
-				</div>
-				<div>
-					<p className='text-lg text-gray-200 mb-3'>Tags</p>
-					<div className='flex gap-x-2 mb-5'>
-						{post &&
-							Object.values(post?.tags).map((tag, index) => (
-								<Tag key={index}>{tag}</Tag>
-							))}
+				{!isEdit ? (
+					<>
+						<div>
+							<p className='text-xl text-white mb-10'>{post?.description}</p>
+						</div>
+						<div>
+							<p className='text-lg text-gray-200 mb-3'>Tags</p>
+							<div className='flex gap-x-2 mb-5'>
+								{post &&
+									Object.values(post?.tags).map((tag, index) => (
+										<Tag key={index}>{tag}</Tag>
+									))}
+							</div>
+						</div>
+						<Button click={() => setIsEdit(true)}>Edit</Button>
+					</>
+				) : (
+					<div className='flex flex-col gap-y-4 max-w-[40%]'>
+						<div className='flex items-center gap-x-3 justify-between '>
+							<Label labelContent='Post name' />
+							<Input
+								placeholder='Post Name'
+								onChange={(e) => setPostName(e.target.value)}
+							/>
+						</div>
+						<div className='flex items-center gap-x-3 justify-between'>
+							<Label labelContent='Post Content' />
+							<Input
+								placeholder='Post Content'
+								onChange={(e) => setPostDescription(e.target.value)}
+							/>
+						</div>
+						<div className='flex items-center gap-x-3 justify-between'>
+							<Label labelContent='Post Content' />
+							<Input
+								placeholder='Post Tag'
+								onChange={(e) => setPostTags(e.target.value.split(','))}
+							/>
+						</div>
+						<Button click={handleEditPost}>Ok</Button>
 					</div>
-				</div>
+				)}
 			</div>
 		</section>
 	);
