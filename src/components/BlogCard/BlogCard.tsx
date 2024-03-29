106657ee9954e39e178ec761f168ab4efb69c34d
kwinkich <vankord21@gmail.com>
2024-03-29 11:48:53 +0200

    fix any UI bug & added page for News

--- src/components/BlogCard/BlogCard.tsx
+++ src/components/BlogCard/BlogCard.tsx
@@ -11,11 +11,13 @@ interface BlogCardProps {
 export const BlogCard: React.FC<BlogCardProps> = ({ data }) => {
 	return (
 		<Link to={`/post/${data._id}`}>
-			<div className='border-2 border-gray-100 w-[355px] hover:bg-neutral-900 duration-300 ease-in-out'>
+			<div className='border-2 border-gray-100 w-[450px] hover:bg-neutral-900 duration-300 ease-in-out'>
 				<div className='flex flex-col px-10 py-8'>
 					<h3 className='text-2xl text-white font-semibold'>{data.title}</h3>
-					<p className='text-lg text-white mb-3'>{data.description}</p>
-					<div className='flex gap-x-2 mb-5'>
+					<p className='text-lg text-white mb-3 line-clamp-2'>
+						{data.description}
+					</p>
+					<div className='flex flex-wrap gap-x-2 gap-y-2 mb-5'>
 						{Object.values(data.tags).map((tag, index) => (
 							<Tag key={index}>{tag}</Tag>
 						))}
--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,34 +1,77 @@
-// import { useState } from 'react';
-// import { Link, useParams } from 'react-router-dom';
-// import { Button } from '../components/Button/Button';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Button } from '../components/Button/Button';
 import { Header } from '../components/Header/Header';
-// import { Input } from '../components/Input/Input';
-// import { Label } from '../components/Label/Label';
+import { Input } from '../components/Input/Input';
+import { Label } from '../components/Label/Label';
+import { News } from '../types/News';
 
 export default function NewsPage() {
-	// const [isEdit, setIsEdit] = useState<boolean>(false);
-	// const [newsName, setNewsName] = useState<string>('');
-	// const [newsDescription, setNewsDescription] = useState<string>('');
-	// const { id } = useParams();
+	const [newsData, setNewsData] = useState<News>();
+	const [isEdit, setIsEdit] = useState<boolean>(false);
+	const [newsTitle, setNewsTitle] = useState<string>('');
+	const [newsDescription, setNewsDescription] = useState<string>('');
+	const { id } = useParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const getNewsById = async () => {
+			try {
+				const newData = await axios.get(`http://localhost:5050/api/news/${id}`);
+				setNewsData(newData.data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+
+		getNewsById();
+	}, []);
+
+	const handleEditNews = async () => {
+		try {
+			setIsEdit(false);
+			const updatePost = await axios.put(
+				`http://localhost:5050/api/news/update/${id}`,
+				{
+					title: newsTitle,
+					description: newsDescription,
+				}
+			);
+			setNewsData(updatePost.data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const handleDeleteNews = async () => {
+		try {
+			const deletedPost = await axios.delete(
+				`http://localhost:5050/api/news/delete/${id}`
+			);
+			console.log(deletedPost);
+			return navigate('/');
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
 	return (
 		<section className='section'>
 			<Header />
 			<div>
-				{/* <Link to={`/`}> */}
-				<p className='back'> &lt; back</p>
-				{/* </Link> */}
-				{/* <h1 className='h1 mb-10'>{news?.name}</h1> */}
-				{/* {!isEdit ? (
+				<Link to={`/`}>
+					<p className='back'> &lt; back</p>
+				</Link>
+				<h1 className='h1 mb-10'>{newsData?.title}</h1>
+				{!isEdit ? (
 					<>
 						<div>
-							<p className='description'>{news?.description}</p>
+							<p className='description'>{newsData?.description}</p>
 						</div>
 						<div className='flex gap-x-3 items-center'>
 							<Button click={() => setIsEdit(true)}>Edit</Button>
-							<Link to={`/`}>
-								<Button click={() => deleteNews(news?.id || 0)}>Delete</Button>
-							</Link>
+							<Button click={handleDeleteNews}>Delete</Button>
 						</div>
 					</>
 				) : (
@@ -37,7 +80,7 @@ export default function NewsPage() {
 							<Label labelContent='News name' />
 							<Input
 								placeholder='News Name'
-								onChange={(e) => setNewsName(e.target.value)}
+								onChange={(e) => setNewsTitle(e.target.value)}
 							/>
 						</div>
 						<div className='form'>
@@ -49,7 +92,7 @@ export default function NewsPage() {
 						</div>
 						<Button click={handleEditNews}>Ok</Button>
 					</div>
-				)} */}
+				)}
 			</div>
 		</section>
 	);
